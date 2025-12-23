ab3e14b64003bd08cb36f7ad0240e057ad7575f7
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-23 14:17:52 -0500

    test(App): update test expectations for "Starred Characters" and "Characters" to reflect new counts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App'
 describe('App', () => {
   it('renders the app', () => {
     render(<App />)
-    expect(screen.getByText(/Starred Characters/i)).toBeInTheDocument()
-    expect(screen.getByText(/Characters/i)).toBeInTheDocument()
+    expect(screen.getByText(/Starred Characters \(2\)/i)).toBeInTheDocument()
+    expect(screen.getByText(/Characters \(4\)/i)).toBeInTheDocument()
   })
 })
