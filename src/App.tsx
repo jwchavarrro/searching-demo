fef7a7d62309ceb5efc9ebd69e9fb917d130d851
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-23 14:13:58 -0500

    feat(styles): refactor App component styles using Tailwind CSS for improved layout and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,28 @@
  * @description: Este componente gestiona el layout principal de la aplicación.
  */
 
-import './App.css'
-
 function App() {
   return (
-    <div className="flex h-screen w-screen overflow-hidden">
+    <div className="app">
       {/* Sidebar */}
-      <aside className="flex flex-col gap-4 justify-between p-4 grow border md:max-w-80 lg:max-w-96">
-        <header className="border min-h-24">
+      <aside className="sidebar">
+        <header className="sidebar-header">
           header ( Title and Search Bar )
         </header>
 
         {/* Personajes favoritos y personajes */}
-        <main className="border h-full">
-          <section className="border h-1/3">Starred Characters (2)</section>
-          <section className="border h-2/3">Characters (4)</section>
+        <main className="sidebar-main">
+          <section className="sidebar-section-starred">
+            Starred Characters (2)
+          </section>
+          <section className="sidebar-section-characters">
+            Characters (4)
+          </section>
         </main>
       </aside>
 
       {/* Main Content Area */}
-      <main className="hidden grow border md:block"></main>
+      <main className="main-content"></main>
     </div>
   )
 }
