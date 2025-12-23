0db55bbfd15fbcc7e1fa7102ab21a28fb60b91d5
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-23 14:16:01 -0500

    test(App): update test to check for "Starred Characters" and "Characters" in the App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ import App from './App'
 describe('App', () => {
   it('renders the app', () => {
     render(<App />)
-    expect(screen.getByText(/Hello World/i)).toBeInTheDocument()
+    expect(screen.getByText(/Starred Characters/i)).toBeInTheDocument()
+    expect(screen.getByText(/Characters/i)).toBeInTheDocument()
   })
 })
