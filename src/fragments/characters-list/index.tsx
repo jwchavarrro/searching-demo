4091c76b5fdf25f62d5f96fb8163d1a5bb201e2a
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-24 02:12:54 -0500

    refactor(CharactersList): replace useCharacters hook with useFilteredCharacters for improved character filtering; update character count display and handle empty state

--- src/fragments/characters-list/index.tsx
+++ src/fragments/characters-list/index.tsx
@@ -8,7 +8,7 @@ import { CardA, Message } from '@/components/atomic-desing/molecules'
 import { Text } from '@/components/atomic-desing/atoms'
 
 // Import of hooks
-import { useCharacters } from '@/hooks'
+import { useFilteredCharacters } from './hooks'
 import { useSelectedCharacter } from '@/context/use-selected-character'
 import { useCharactersStarred } from '@/context'
 
@@ -20,11 +20,12 @@ import type { CharacterType } from '@/graphql/types'
 
 export function CharactersList() {
   // Implement custom hooks
-  /* @name useCharacters
-  @description: Hook para obtener los personajes  
- */
-  const { data, isLoading, error } = useCharacters()
+  /* @name useFilteredCharacters
+  @description: Hook para obtener los personajes filtrados (excluyendo los starred)
+  */
+  const { filteredCharacters, isLoading, error, data } = useFilteredCharacters()
 
+  // Implement context
   /* @name useSelectedCharacter
   @description: Hook para manejar el estado del personaje seleccionado
   */
@@ -73,30 +74,46 @@ export function CharactersList() {
     )
   }
 
+  if (filteredCharacters.length === 0) {
+    return (
+      <div className="min-h-52">
+        <Message
+          icon={ICONS.alert}
+          description={{ text: 'All characters are starred' }}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+    )
+  }
+
   return (
     <div className="min-h-0 space-y-2">
-      <Text text={`CHARACTERS (${data.results.length})`} weight="semibold" />
+      <Text
+        text={`CHARACTERS (${filteredCharacters.length})`}
+        weight="semibold"
+      />
       <div>
-        {data.results.map((character: CharacterType) => (
-          <CardA
-            key={character.id}
-            as="button"
-            onClick={() => setSelectedCharacter(character)}
-            isStarred={isCharacterStarred(character.id)}
-            onIconClick={() => handleCharacterStarred(character)}
-            avatar={{
-              src: character.image,
-              alt: character.name,
-              size: 'lg',
-            }}
-            title={{
-              title: character.name,
-            }}
-            description={{
-              text: character.species,
-            }}
-          />
-        ))}
+        {filteredCharacters.length > 0 &&
+          filteredCharacters.map((character: CharacterType) => (
+            <CardA
+              key={character.id}
+              as="button"
+              onClick={() => setSelectedCharacter(character)}
+              isStarred={isCharacterStarred(character.id)}
+              onIconClick={() => handleCharacterStarred(character)}
+              avatar={{
+                src: character.image,
+                alt: character.name,
+                size: 'lg',
+              }}
+              title={{
+                title: character.name,
+              }}
+              description={{
+                text: character.species,
+              }}
+            />
+          ))}
       </div>
     </div>
   )
