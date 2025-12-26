e392ba220c4212657e07e408315417f83f416216
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-26 11:19:24 -0500

    feat(header): add star button functionality and improve layout for profile header

--- src/components/atomic-desing/molecules/header/index.tsx
+++ src/components/atomic-desing/molecules/header/index.tsx
@@ -2,9 +2,14 @@
  * Header.tsx
  * @description: Componente molecular que combina Avatar y Title para crear un header de perfil.
  */
+import { Icon } from '@iconify/react'
 
 // Import of components custom
-import { Title, type TitleProps } from '@/components/atomic-desing/atoms'
+import {
+  Button,
+  Title,
+  type TitleProps,
+} from '@/components/atomic-desing/atoms'
 import {
   AvatarButton,
   type AvatarButtonProps,
@@ -13,17 +18,49 @@ import {
 // Import of utils
 import { cn } from '@/utils/cn'
 
+import { ICONS } from '@/config'
+
 export interface HeaderProps {
   avatar: AvatarButtonProps
   title: TitleProps
+  isStarred: {
+    status: boolean
+    onIconClick: () => void
+  }
   className?: string
 }
 
-export const Header = ({ avatar, title, className }: HeaderProps) => {
+export const Header = ({
+  avatar,
+  title,
+  isStarred,
+  className,
+}: HeaderProps) => {
   return (
-    <div className={cn(className)}>
-      <AvatarButton {...avatar} />
-      <Title {...title} size="2xl" />
+    <div className={cn('grid grid-cols-2 items-center gap-5', className)}>
+      {/* Column #1: Avatar and title */}
+      <div>
+        <AvatarButton {...avatar} />
+        <Title {...title} size="2xl" />
+      </div>
+
+      {/* Column #2: Star button */}
+      <div className="flex justify-end">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={isStarred.onIconClick}
+          className="shrink-0 rounded-full transition-colors duration-300 ease-in-out group-hover:bg-white group-hover:shadow hover:bg-white"
+        >
+          <Icon
+            className={cn(
+              'text-gray/50 size-4 md:size-7',
+              isStarred.status && 'text-secondary-600'
+            )}
+            icon={isStarred.status ? ICONS.heart : ICONS.heart_outline}
+          />
+        </Button>
+      </div>
     </div>
   )
 }
