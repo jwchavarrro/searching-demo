a021fe220696bee20617805d93f3cdbe1808fa81
jwchavarrro <jwchavarro@prevalentware.com>
2025-12-27 11:13:51 -0500

    chore(vite): increase chunk size warning limit to 600

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
